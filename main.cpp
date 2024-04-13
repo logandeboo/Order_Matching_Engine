8851c94eb6a16317189cfb8dc975285de5103b01
pastjacket <log.deboo@gmail.com>
2024-04-13 01:59:41 -0500

    Init repo

--- main.cpp
+++ main.cpp
@@ -9,18 +9,6 @@ enum class OrderType
 
 int main()
 {
-
-    int x = 4;
-
-    int *y = &x;
-
-    // std::cout << "The value of x is " << x << std::endl;
-    // std::cout << "The value of y is " << y << std::endl;
-    // std::cout << "The value of &y is " << &y << std::endl;
-    // std::cout << "The value of *y is " << *y << std::endl;
-
-    // std::cout << "BUY: " << OrderType::BUY << "and SELL: " << OrderType::SELL << std::endl;
-
     std::cout << "BUY: " << "and SELL: " << std::endl;
 
     return 0;
