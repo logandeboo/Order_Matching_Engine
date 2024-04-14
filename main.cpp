4034a8fd27bbc89ce89659a3e7ecb4df3eca2603
pastjacket <log.deboo@gmail.com>
2024-04-13 20:34:21 -0500

    First Commit

--- main.cpp
+++ main.cpp
@@ -7,9 +7,32 @@ enum class OrderType
     SELL
 };
 
+class Order
+{
+
+    // Public and private are access specifiers used to control the visibility of class members
+    // (variables and functions) within a class. Private members are accessible only within the same class.
+    // This ensures that the internal state of the class cannot be modified from the outside and hides
+    // implementation details. Public members are accessible from anywhere in the program. Public memebrs define
+    // the interface to the class providing methods for interacting with its objects.
+private:
+    std::string orderId;
+    long timestamp;
+    OrderType type;
+    double price;
+    int quantity;
+
+public:
+};
+
 int main()
 {
-    std::cout << "BUY: " << "and SELL: " << std::endl;
 
+    std::cout << "Please enter you name: ";
+
+    std::string name;
+    std::cin >> name;
+
+    std::cout << "Hello, " << name << std::endl;
     return 0;
 }
