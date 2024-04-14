b67decfe6a2486fea3b86bec6c06c263c5425b5c
pastjacket <log.deboo@gmail.com>
2024-04-13 21:12:01 -0500

    Reorganized file structure

--- main.cpp
+++ main.cpp
@@ -17,22 +17,50 @@ class Order
     // the interface to the class providing methods for interacting with its objects.
 private:
     std::string orderId;
-    long timestamp;
-    OrderType type;
+    std::string timestamp;
+    std::string assetName;
+    OrderType orderType;
     double price;
     int quantity;
 
 public:
+    Order(std::string orderId, std::string timestamp, std::string assetName, OrderType orderType, double price, int qty) : orderId(orderId), orderType(orderType), price(price), quantity(qty) {}
+
+    // Getters
+    std::string get_order_id() const
+    {
+        return orderId;
+    }
+    std::string get_timestamp() const
+    {
+        return timestamp;
+    }
+
+    std::string get_asset_name() const
+    {
+        return assetName;
+    }
+
+    OrderType get_order_type() const
+    {
+        return orderType;
+    }
+
+    double get_price() const
+    {
+        return price;
+    }
+
+    int get_quantity() const
+    {
+        return quantity;
+    }
 };
 
 int main()
 {
 
-    std::cout << "Please enter you name: ";
+    Order buyOrder("af234b", "2024-04-12 10:30:00", "MSFT", OrderType::BUY, 123.98, 199);
 
-    std::string name;
-    std::cin >> name;
-
-    std::cout << "Hello, " << name << std::endl;
-    return 0;
+    std::cout << buyOrder.get_order_id() << std::endl;
 }
